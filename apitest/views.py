709969e8df484a7f5d64842a6a74346044bf511e
susu <31542614@qq.com>
2019-11-20 11:00:08 +0800

    登录功能

--- apitest/views.py
+++ apitest/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
+from django.contrib import auth
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 def test(request):
     return HttpResponse("hello test")  #返回 HttpResponse 响应函数
+def login(request):
+    if request.POST:
+        username=password=""
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user=auth.authenticate(username=username,password=password)
+        if user is not None and user.is_active:
+            auth.login(request,user)
+            request.session['user']=username
+            response=HttpResponseRedirect("/home/")
+            return  response
+        else:
+            return render(request,'login.html',{"error":"username or password error"})
+
+    return render(request,'login.html')
+def home(request):
+    return render(request,"home.html")
--- autotest/settings.py
+++ autotest/settings.py
@@ -75,10 +75,12 @@ WSGI_APPLICATION = 'autotest.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': {'ENGINE': 'django.db.backends.mysql', 'NAME': 'autotest', 'USER': 'root', 'PASSWORD': 'gao123456',
+                'HOST': '192.168.99.100', 'PORT': '3306', }
 }
 
 
